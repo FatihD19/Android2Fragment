8f3fc5538dc57162b4aef1eae6989be271dbce9e
FatihD19 <89921102+FatihD19@users.noreply.github.com>
2022-02-11 23:06:43 +0700

    Update SimpleFragment.java

--- app/src/main/java/com/example/fragmentexample1updated/SimpleFragment.java
+++ app/src/main/java/com/example/fragmentexample1updated/SimpleFragment.java
@@ -1,12 +1,17 @@
 package com.example.fragmentexample1updated;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,14 +25,36 @@ public class SimpleFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+    private  static final String CHOICE_PARAM = "choice-param";
+    private static final int NONE = 2;
+    private int nCurrentChioice = NONE;
+
+    private OnFragmentInteractionListener mListener;
+    interface OnFragmentInteractionListener{
+        void onRadioButtonChoiceChecked(int choice);
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if(context instanceof OnFragmentInteractionListener){
+            mListener = (OnFragmentInteractionListener) context;
+        }
+    }
+
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
+    private static final int YES = 1;
+    private static final int NO = 0;
     public SimpleFragment() {
         // Required empty public constructor
     }
-
+    public static SimpleFragment newInstance(){
+        SimpleFragment fragment = new SimpleFragment();
+        return fragment;
+    }
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
@@ -45,7 +72,13 @@ public class SimpleFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
-
+    public static SimpleFragment newInstance(int choice){
+        SimpleFragment fragment = new SimpleFragment();
+        Bundle args = new Bundle();
+        args.putInt(CHOICE_PARAM,choice);
+        fragment.setArguments(args);
+        return fragment;
+    }
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,6 +92,40 @@ public class SimpleFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_simple, container, false);
+        View view = inflater.inflate(R.layout.fragment_simple, container, false);
+        TextView questionTextView = view.findViewById(R.id.fragment_header);
+
+        RadioGroup radioGroup = view.findViewById(R.id.radio_group);
+
+        if(getArguments().containsKey(CHOICE_PARAM)){
+            nCurrentChioice =getArguments().getInt(CHOICE_PARAM);
+            if(nCurrentChioice !=NONE){
+                radioGroup.check(radioGroup.getChildAt(nCurrentChioice).getId());
+            }
+        }
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                RadioButton checkedButton = radioGroup.findViewById(i);
+                int idx = radioGroup.indexOfChild(checkedButton);
+
+                if (idx == YES){
+                    questionTextView.setText(R.string.yes_message);
+                    nCurrentChioice = YES;
+                    mListener.onRadioButtonChoiceChecked(YES);
+                }
+                else if(idx == NO){
+                    questionTextView.setText(R.string.no_message);
+                    nCurrentChioice = NO;
+                    mListener.onRadioButtonChoiceChecked(NO);
+                }
+                else{
+                    nCurrentChioice=NONE;
+                    mListener.onRadioButtonChoiceChecked(NONE);
+                }
+            }
+        });
+        return view;
     }
 }
